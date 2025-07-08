108a67b3c84fe8513ec139afd5f66de1560caabd
hdz-kevin <kevinhdez592@gmail.com>
2025-07-08 15:10:38 -0600

    move routes to routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/patients', fn () => view('patients.index'))->name('patients.index');
 });
 
 require __DIR__.'/auth.php';
