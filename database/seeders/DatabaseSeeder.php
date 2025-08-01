89f89854f88ddffd681eaea20fd55148b8b33981
hdz-kevin <kevinhdez592@gmail.com>
2025-08-01 17:25:01 -0600

    load appointment seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PatientSeeder::class,
             DoctorSeeder::class,
+            AppointmentSeeder::class,
         ]);
     }
 }
